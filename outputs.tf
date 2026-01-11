91ebe24348d00df1d184ba7089c9457c2b1f9d9b
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:52:50 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "network_manager_admin_rule_collections" {
+  description = "All network_manager_admin_rule_collection resources"
+  value       = azurerm_network_manager_admin_rule_collection.network_manager_admin_rule_collections
+}
+
